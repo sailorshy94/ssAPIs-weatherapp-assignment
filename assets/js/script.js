0445f3b67c263e51da57e0e5a78b3105af22cae6
sailorshy94 <sbutle94@gmail.com>
2023-11-27 17:47:57 -0500

    added comments and commented out code for fetch req for later use while beginning to think about logical order of webpage function

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,30 @@
 */
 
 // want to be able to use JS to make cities searched persist under submit button
+
+// appid = e3762bdf720c3dc3229b0039e02fa026
+// API Call for geocoding - allows user to type name in and get api response
+// http://api.openweathermap.org/geo/1.0/direct?q={city name}&limit={limit}&appid={API key}
+// limit = 1
+// http://api.openweathermap.org/geo/1.0/direct?q={city name}&limit=1&appid=e3762bdf720c3dc3229b0039e02fa026
+
+// API Call - Current Weather Data
+// https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
+
+// API Call - 5 day/ 3 hr forecast data
+// api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
+
+// fetch ("")
+//    .then(function(response) {
+//     return response.json();
+//    }) 
+//    .then(function(data) {
+//     console.log()
+//     console.log(data);
+//    });
+
+// function getGeoCoordApi() {
+//     var requestUrl = "";
+
+//     fetch(requestUrl)
+// }
